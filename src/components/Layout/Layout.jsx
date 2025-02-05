ca0630436e1d04f0098984e47e0b54139d7ee3c8
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-02-05 18:10:08 +0200

    Add header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,9 @@ import { useDispatch } from "react-redux";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { logout } from "../../redux/auth/authActions";
 
+import { BurgerMenuIcon, MainLogoIcon } from "../Icons";
+import css from "./Layout.module.css";
+
 export const Layout = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -13,21 +16,57 @@ export const Layout = () => {
   };
 
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
+    <div className={css.containerLayout}>
+      <header className={css.header}>
+        <div className={css.containerMainLogo}>
+          <MainLogoIcon />
+          <p className={css.mainLogoText}>read journey</p>
+        </div>
+
+        <nav className={css.nav}>
+          <ul className={css.navList}>
+            <li className={css.navItem}>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? `${css.navLink} ${css.active}` : css.navLink
+                }
+              >
+                Home
+              </NavLink>
             </li>
-            <li>
-              <NavLink to="/library">My library</NavLink>
+            <li className={css.navItem}>
+              <NavLink
+                to="/library"
+                className={({ isActive }) =>
+                  isActive ? `${css.navLink} ${css.active}` : css.navLink
+                }
+              >
+                My library
+              </NavLink>
             </li>
           </ul>
         </nav>
-        <button type="button" onClick={handleLogout}>
-          Logout
-        </button>
+
+        <div className={css.containerLine}>
+          <div className={css.containerUserInfo}>
+            <div className={css.userAvatar}>A</div>
+            <p className={css.userName}>User Name</p>
+          </div>
+
+          <button
+            className={css.buttonLogout}
+            type="button"
+            onClick={handleLogout}
+          >
+            Log out
+          </button>
+
+          <button className={css.buttonBurger} type="button">
+            {" "}
+            <BurgerMenuIcon />
+          </button>
+        </div>
       </header>
 
       <main>
