bb5a3eba691e95736a47e22f008e1e2baac96cec
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-02-11 18:30:02 +0200

    Add styles in MyLibrary for mobile

--- src/components/MyLibrary/MyLibrary.jsx
+++ src/components/MyLibrary/MyLibrary.jsx
@@ -1,7 +1,8 @@
-// import { useState } from "react";
 import Dashboard from "../Dashboard/Dashboard";
-import {Dropdown} from "../Dropdown/Dropdown";
+import { Dropdown } from "../Dropdown/Dropdown";
+import books from "../../assets/images/books@2x.png";
 import css from "./MyLibrary.module.css";
+import { DeleteBtnIcon } from "../Icons";
 
 const MyLibrary = () => {
   return (
@@ -12,9 +13,37 @@ const MyLibrary = () => {
           <h3 className={css.myLibraryTitle}>My library</h3>
 
           <div className={css.dropdownContainer}>
-            <Dropdown/>
+            <Dropdown />
           </div>
         </div>
+
+        <div className={css.containerBooks}>
+          <div className={css.subcontainerBooks}>
+            <img className={css.booksImg} src={books} alt="books" />
+          </div>
+
+          <p className={css.booksText}>
+            To start training, add{" "}
+            <span className={css.booksTextSpan}>some of your books</span> or
+            from the recommended ones
+          </p>
+        </div>
+
+        <ul className={css.addedBooksList}>
+          <li className={css.addedBooksItem}>
+            <img src="" alt="book" />
+            <div className={css.containerBookInfo}>
+              <div className={css.subcontainerBookInfo}>
+                <p className={css.bookInfoTitle}>Book title</p>
+                <p className={css.bookInfoAuthor}>Author</p>
+              </div>
+
+              <button type="button" className={css.deleteBtnContainer}>
+                <DeleteBtnIcon />
+              </button>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
