b15b2750b51dd7fe68e0151da2d4b575b4afb409
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2025-02-14 11:00:48 +0200

    Update AddBookModal

--- src/components/AddBookModal/AddBookModal.jsx
+++ src/components/AddBookModal/AddBookModal.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { CloseBtnIcon } from "../Icons";
+import thumbUpImg from "../../assets/images/thumb-up@2x.png";
 import css from "./AddBookModal.module.css";
 import PropTypes from "prop-types";
 import { addBookToLibrary } from "../../services/api";
 
 const AddBookModal = ({ book, onClose, onAddBook }) => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
   const handleOverlayClick = (e) => {
     if (e.target === e.currentTarget) {
       onClose();
@@ -15,8 +19,7 @@ const AddBookModal = ({ book, onClose, onAddBook }) => {
     try {
       await addBookToLibrary(book._id);
       onAddBook(book);
-      alert("Book successfully added!");
-      onClose();
+      setIsSuccess(true);
     } catch {
       alert("Failed to add book. Please try again.");
     }
@@ -25,18 +28,47 @@ const AddBookModal = ({ book, onClose, onAddBook }) => {
   return (
     <div className={css.overlayAddModal} onClick={handleOverlayClick}>
       <div className={css.modal} onClick={(e) => e.stopPropagation()}>
-        <button className={css.closeButton} onClick={onClose}>
-          <CloseBtnIcon className={css.closeIcon} />
-        </button>
-        <div className={css.containerModalInfo}>
-          <img className={css.bookImg} src={book.imageUrl} alt={book.title} />
-          <h3 className={css.bookTitle}>{book.title}</h3>
-          <p className={css.bookAuthor}>{book.author}</p>
-          <p className={css.pages}>{book.totalPages} pages</p>
-          <button className={css.addBtn} type="button" onClick={handleAddBook}>
-            Add to library
-          </button>
-        </div>
+        {!isSuccess ? (
+          <>
+            <button className={css.closeButton} onClick={onClose}>
+              <CloseBtnIcon className={css.closeIcon} />
+            </button>
+            <div className={css.containerModalInfo}>
+              <img
+                className={css.bookImg}
+                src={book.imageUrl}
+                alt={book.title}
+              />
+              <h3 className={css.bookTitle}>{book.title}</h3>
+              <p className={css.bookAuthor}>{book.author}</p>
+              <p className={css.pages}>{book.totalPages} pages</p>
+              <button
+                className={css.addBtn}
+                type="button"
+                onClick={handleAddBook}
+              >
+                Add to library
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className={css.successfullAdded}>
+            <button
+              className={css.closeBtnSuccess}
+              type="button"
+              onClick={onClose}
+            >
+              <CloseBtnIcon className={css.closeIconSuccess} />
+            </button>
+            <img className={css.thumbUpImg} src={thumbUpImg} alt="thumbup" />
+            <h3 className={css.successfullAddedTitle}>Good job</h3>
+            <p className={css.successfullAddedText}>
+              Your book is now in{" "}
+              <span className={css.successfullTextSpan}>the library!</span> The
+              joy knows no bounds and now you can start your training
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -55,59 +87,3 @@ AddBookModal.propTypes = {
 };
 
 export default AddBookModal;
-
-// import { CloseBtnIcon } from "../Icons";
-// import css from "./AddBookModal.module.css";
-// import PropTypes from "prop-types";
-// import { addBookToLibrary } from "../../services/api";
-
-// const AddBookModal = ({ book, onClose }) => {
-//   const handleOverlayClick = (e) => {
-//     if (e.target === e.currentTarget) {
-//       onClose();
-//     }
-//   };
-
-//   const handleAddBook = async () => {
-//     console.log("Book object:", book);
-//     try {
-//       await addBookToLibrary(book._id);
-//       alert("Book successfully added!");
-//       onClose();
-//     } catch {
-//       alert("Failed to add book. Please try again.");
-//     }
-//   };
-
-//   return (
-//     <div className={css.overlayAddModal} onClick={handleOverlayClick}>
-//       <div className={css.modal} onClick={(e) => e.stopPropagation()}>
-//         <button className={css.closeButton} onClick={onClose}>
-//           <CloseBtnIcon className={css.closeIcon} />
-//         </button>
-//         <div className={css.containerModalInfo}>
-//           <img className={css.bookImg} src={book.imageUrl} alt={book.title} />
-//           <h3 className={css.bookTitle}>{book.title}</h3>
-//           <p className={css.bookAuthor}>{book.author}</p>
-//           <p className={css.pages}>{book.totalPages} pages</p>
-//           <button className={css.addBtn} type="button" onClick={handleAddBook}>
-//             Add to library
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// AddBookModal.propTypes = {
-//   book: PropTypes.shape({
-//     _id: PropTypes.string.isRequired,
-//     imageUrl: PropTypes.string.isRequired,
-//     title: PropTypes.string.isRequired,
-//     author: PropTypes.string.isRequired,
-//     totalPages: PropTypes.number.isRequired,
-//   }).isRequired,
-//   onClose: PropTypes.func.isRequired,
-// };
-
-// export default AddBookModal;
